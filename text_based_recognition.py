f6de6d872ec84bde30a31ce33a592095578e2918
MHADHBImanel <MHADHBImanel@github.com>
2018-03-09 16:24:50 +0000

    update

--- text_based_recognition.py
+++ text_based_recognition.py
@@ -3,13 +3,11 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
-# Path of working folder on Disk
-src_path = "E:/Lab/Python/Project/OCR/"
+
 
 def get_string(img_path):
     # Read image with opencv
-    img = cv2.imread(img_path)
-
+    img = cv2.imread(img_path) 
     # Convert to gray
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -19,16 +17,16 @@ def get_string(img_path):
     img = cv2.erode(img, kernel, iterations=1)
 
     # Write image after removed noise
-    cv2.imwrite(src_path + "removed_noise.png", img)
+    cv2.imwrite("removed_noise.jpg", img)
 
     #  Apply threshold to get image with only black and white
     #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
     # Write the image after apply opencv to do some ...
-    cv2.imwrite(src_path + "thres.png", img)
+    cv2.imwrite("thres.jpg", img)
 
     # Recognize text with tesseract for python
-    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+    result = pytesseract.image_to_string(Image.open("im1.jpg"))
 
     # Remove template file
     #os.remove(temp)
@@ -36,7 +34,7 @@ def get_string(img_path):
     return result
 
 
-print '--- Start recognize text from image ---'
-print get_string(src_path + "2.png")
+print ('--- Start recognize text from image ---')
+print (get_string("im1.jpg"))
 
-print "------ Done -------"
+print ("------ Done -------")
